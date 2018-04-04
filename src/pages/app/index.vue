2d2c32467b3666344e5f91c8a886cd101a74dff3
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-04-04 16:06:08 -0300

    Fix flow chart

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -1,12 +1,11 @@
 <template>
-
 <div class="">
-  <button type="button" name="button" @click="resetPosition()" class="btn btn-reset">reset</button>
 
-  <div class="app container align-center text-align-center" id="appc">
+  <div class="app container text-align-center" id="appc">
     <div class="app-master" ref="appmaster">
       <div class="app-content" ref="role">
-        <tree :text="father.text" :nodes="father.childrens" ref="role"/> </div>
+        <tree :text="father.text" :nodes="father.childrens" ref="role" />
+      </div>
     </div>
   </div>
 
@@ -21,37 +20,25 @@ export default {
       father: {
         text: 'Bichinhos',
         first: true,
-        childrens: [
-          {
-            text: 'Home'
-          },
-          {
-            text: 'Features',
-            childrens: [
-              {
-                text: 'Feature 1'
-              },
-              {
-                text: 'Feature 2',
-                childrens: [
-                  {
-                    text: 'Feature 2 1'
-                  },
-                  {
-                    text: 'Feature 2 2'
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            text: 'Mapa'
-          },
-          {
-            text: 'Contato'
-          }
-        ]
-
+        childrens: [{
+          text: 'Home'
+        }, {
+          text: 'Features',
+          childrens: [{
+            text: 'Feature 1'
+          }, {
+            text: 'Feature 2',
+            childrens: [{
+              text: 'Feature 2 1'
+            }, {
+              text: 'Feature 2 2'
+            }]
+          }]
+        }, {
+          text: 'Mapa'
+        }, {
+          text: 'Contato'
+        }]
       }
     }
   },
@@ -63,16 +50,15 @@ export default {
   },
   methods: {
     resetPosition: function () {
-      console.log('_________')
-      const app = this.$refs['appmaster']
-      const blockSize = app.clientWidth / 2
-      console.log('l ', app.offsetLeft)
-
-      console.log(blockSize)
-      const total = app.offsetLeft / 2
-      console.log('total ', total)
-
-      window.scrollTo(total, (app.offsetTop))
+      // console.log('_________')
+      // const app = this.$refs['appmaster']
+      // console.log('app left ', app.offsetLeft)
+      // const blockSize = app.clientWidth
+      // console.log(blockSize)
+      //
+      // const total = app.offsetLeft / 2
+      //
+      // window.scrollTo(total, (app.offsetTop))
     }
   }
 }
@@ -80,8 +66,11 @@ export default {
 
 <style lang="scss" scoped>
 .app {
-    min-width: 200vw;
-    min-height: 200vh;
+    width: 100vw;
+    height: 100vh;
+    overflow: auto;
+    box-sizing: border-box;
+    // background: red;
 }
 
 .btn {
@@ -92,9 +81,16 @@ export default {
     position: fixed;
     top: 10px;
     left: 10px;
+    box-shadow: 0 0 30px 0 rgba(0, 0, 0, .5);
+    padding: 0;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
 }
 
 .app-master {
+    padding: 10vw;
     box-shadow: 0 0 0 2px red;
+    margin: 0 auto;
 }
 </style>
