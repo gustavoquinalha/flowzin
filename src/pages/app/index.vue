07f84cf66ddb103efc006eecb1e10ccd54b96d43
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-04-04 11:41:39 -0300

    Init project

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="app">
+<div class="app container align-center text-align-center">
 
-    <div class="btn">
+  <div class="app--content">
+
+    <div class="btn"
+         id="role"
+         ref="role">
       Start
     </div>
 
   </div>
+
+</div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    // window.location.hash = '#role'
+    const div = this.$refs['role']
+    console.log(div.offsetLeft)
+    console.log(div.offsetRight)
+    console.log(div.offsetTop)
+    console.log(div.offsetBottom)
+
+    // this.$refs['role'].focus()
+    // console.log(this.$refs['role'])
+  }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.app {
+    min-width: 200vw;
+    min-height: 200vh;
+    background: red;
+    padding: 100px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import app from '@/pages/app'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
